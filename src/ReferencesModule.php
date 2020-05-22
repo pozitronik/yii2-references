b2e1cb76b22c2e4ce9f5286f69950753991bce4a
Pozitronik <antikillerxxl@gmail.com>
2020-05-22 21:48:27 +0300

    use Module instead of CoreModule

--- src/ReferencesModule.php
+++ src/ReferencesModule.php
@@ -3,14 +3,14 @@ declare(strict_types = 1);
 
 namespace pozitronik\references;
 
-use pozitronik\core\models\core_module\CoreModule;
 use Yii;
+use yii\base\Module;
 
 /**
  * Class ReferencesModule
  * @package app\modules\references
  */
-class ReferencesModule extends CoreModule {
+class ReferencesModule extends Module {
 
 	/**
 	 * {@inheritDoc}
