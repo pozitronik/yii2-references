b8147b478a3c09d0ae0df066f24f319defd3cfe9
Pozitronik <antikillerxxl@gmail.com>
2021-05-11 17:52:33 +0300

    Выглядело неправильно

--- src/widgets/reference_select/ReferenceSelectWidget.php
+++ src/widgets/reference_select/ReferenceSelectWidget.php
@@ -52,7 +52,7 @@ class ReferenceSelectWidget extends Select2 {
 			if ($this->showEditAddon) {
 				$this->addon = [
 					'append' => [
-						'content' => ReferencesModule::a("<i class='fa fa-edit ' title='Редактирование'></i>", ['references/index', 'class' => ReflectionHelper::GetClassShortName((string)$this->referenceClass)], ['class' => 'btn btn-default']),
+						'content' => ReferencesModule::a("<i class='fa fa-edit ' title='Редактирование'></i>", ['references/index', 'class' => ReflectionHelper::GetClassShortName(get_class($this->referenceClass))], ['class' => 'btn btn-default']),
 						'asButton' => true
 					]
 				];
