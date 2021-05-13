e9a7a0a7aca49c48b0d05b024abbe3793a251759
Pozitronik <antikillerxxl@gmail.com>
2021-05-13 12:28:02 +0300

    Reference migrations

---
+++ src/models/ReferenceMigration.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types = 1);
+
+namespace pozitronik\references\models;
+
+use yii\db\Migration;
+
+/**
+ * Class ReferenceMigration
+ * @property string $table_name
+ */
+class ReferenceMigration extends Migration {
+
+	public string $table_name;
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function safeUp() {
+		$this->createTable($this->table_name, [
+			'id' => $this->primaryKey(),
+			'name' => $this->string(255)->notNull(),
+			'deleted' => $this->boolean()->notNull()->defaultValue(false)
+		]);
+
+		$this->createIndex('deleted', $this->table_name, 'deleted');
+		$this->createIndex('name', $this->table_name, 'name');
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function safeDown() {
+		$this->dropTable($this->table_name);
+	}
+}
