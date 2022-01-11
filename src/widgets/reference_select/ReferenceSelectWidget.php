59d97d55dec70b035a0c1be2341ee44e80d28ac9
Pozitronik <antikillerxxl@gmail.com>
2022-01-11 16:02:33 +0300

    deprecated call replaced

--- src/widgets/reference_select/ReferenceSelectWidget.php
+++ src/widgets/reference_select/ReferenceSelectWidget.php
@@ -54,7 +54,7 @@ class ReferenceSelectWidget extends Select2 {
 			if ($this->showEditAddon) {
 				$this->addon = [
 					'append' => [
-						'content' => ReferencesModule::a($this->isBs4()
+						'content' => ReferencesModule::a($this->isBs(4)
 							?"<i class='fa fa-edit' title='Редактирование'></i>"
 							:"<i class='glyphicon glyphicon-wrench' title='Редактирование'></i>",
 							['references/index', 'class' => ReflectionHelper::GetClassShortName($this->referenceClass)], ['class' => 'btn btn-default']),
